3acf4a9881c5b8de1553d0a60f591847cb3ea463
mounika <mounikareddybeerelli@gmail.com>
2019-08-04 11:14:27 +0530

    changed the title

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import DocumentTitle from 'react-document-title';
 
 
 
 ReactDOM.render(
   <BrowserRouter>
-  <DocumentTitle title='MERN APP'>
-
-</DocumentTitle>
-  <App />
+ <App />
   </BrowserRouter>, document.getElementById('root'));
 
 serviceWorker.unregister()
