2a162995d9905bc85e2fb8d2ecc18bfe3547d54f
mounika <mounikareddybeerelli@gmail.com>
2019-08-04 21:24:07 +0530

    Added logging support to track api logs

--- api/models/student.js
+++ api/models/student.js
@@ -1,55 +1,20 @@
 const mongoose =require('mongoose');
-const unique = require('mongoose-unique-validator');
-const validate = require('mongoose-validator');
-mongoose.set('useCreateIndex', true);
-
-
-const student_IdValidator = [
-  validate({
-    validator: 'isLength',
-    arguments: [0, 4],
-    message: 'Name must not exceed {ARGS[1]} characters.'
-  })
-];
-const nameValidator = [
-  validate({
-    validator: 'isLength',
-    arguments: [0, 40],
-    message: 'Name must not exceed {ARGS[1]} characters.'
-  })
-];
-const addressValidator = [
-  validate({
-    validator: 'isLength',
-    arguments: [0, 40],
-    message: 'Address must not exceed {ARGS[1]} characters.'
-  })
-];
 
 const Schema = mongoose.Schema;
-
 
 // Define collection and schema for Student
 let Student = new Schema({
   student_Id: {
-    type: Number, 
-    required: [true, 'Id id required.'],
-    unique: true,
-    validate: student_IdValidator
+    type: Number
   },
   name: {
-    type: String,
-    required: [true, 'Name is required.'],
-    validate: nameValidator
+    type: String
   },
   address: {
-    type: String,
-    required: [true, 'Address is required'],
-    validate: addressValidator
+    type: String
   }, 
 },{
     collection: 'student'
 });
 
-Student.plugin(unique, { message: 'That {PATH} is already taken.' });
 module.exports = mongoose.model('Student', Student);
--- api/routes/student.js
+++ api/routes/student.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const studentRoutes = express.Router();
+const log4js = require('log4js');
+log4js.configure({
+  appenders: { mern: { type: 'file', filename: 'mern.log' } },
+  categories: { default: { appenders: ['mern'], level: 'debug' } }
+});
+ 
+const logger = log4js.getLogger('mern');
 
 // Require student model in our routes module
 let Student = require('../models/student');
@@ -9,10 +16,12 @@ studentRoutes.route('/add').post(function (req, res) {
   let student = new Student(req.body);
   student.save()
     .then(student => {
-      res.status(200).json({'message': 'student is added successfully'});
+      res.status(200).json({'message': 'student is added successfully'}); 
+      logger.info('Student added');    
     })
     .catch(err => {
       res.status(500).json({"Code":"DUPLICATE KEY", "message":err.errmsg});
+      logger.error('Unable to add student');
 
     });
 });
@@ -21,9 +30,11 @@ studentRoutes.route('/add').post(function (req, res) {
 studentRoutes.route('/').get(function (req, res) {
     Student.find(function(err, students){
     if(err){
+      logger.error('Unable to get data');
       console.log(err);
     }
     else {
+      logger.info('get data is called');
       res.json(students);
     }
   });
@@ -33,6 +44,7 @@ studentRoutes.route('/').get(function (req, res) {
 studentRoutes.route('/edit/:id').get(function (req, res) {
   let id = req.params.id;
   Student.findById(id, function (err, student){
+    logger.info('edit data called');
       res.json(student);
   });
 });
@@ -47,10 +59,11 @@ studentRoutes.route('/update/:id').post(function (req, res) {
         student.name = req.body.name;
         student.address = req.body.address;
         student.save().then(student => {
+          logger.info('Student data updated');
           res.status(200).json({'message': 'student is updataed successfully'});
-
         })
       .catch(err => {
+        logger.error('unable to update data');
             res.status(500).send("unable to update the database");
       });
     }
@@ -60,8 +73,14 @@ studentRoutes.route('/update/:id').post(function (req, res) {
 // Defined delete | remove | destroy route
 studentRoutes.route('/delete/:id').get(function (req, res) {
     Student.findByIdAndRemove({_id: req.params.id}, function(err, student){
-        if(err) res.json(err);
-        else res.json('Successfully removed');
+        if(err) {
+          logger.error('unble to delete data');
+        res.json(err);
+        }
+        else {
+          logger.info('student data deleted');
+         res.json('Successfully removed');
+        }
     });
 });
 
